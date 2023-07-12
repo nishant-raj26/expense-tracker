a6d7422de043ef0053da26abdf58d2431fbcf8e3
Nishant <Nishantraj87726@gmail.com>
2023-07-12 12:18:53 +0530

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./assets/components/Header";
 import './App.css'
+import Balance from "./assets/components/Balance";
+import IncomeExpenses from "./assets/components/IncomeExpenses";
+import TransactionList from "./assets/components/TransactionList";
+import AddTransaction from "./assets/components/AddTransaction";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTransaction />
     </div>
   );
 };
